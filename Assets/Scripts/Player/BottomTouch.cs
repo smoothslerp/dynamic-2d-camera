f5c8be542ddc09f18fcf84c7790126ac12cbdc55
alibhojani <alibhojani2@gmail.com>
2020-07-19 23:22:12 -0400

    better player bottom touch

--- Assets/Scripts/Player/BottomTouch.cs
+++ Assets/Scripts/Player/BottomTouch.cs
@@ -3,20 +3,27 @@
 public class BottomTouch : MonoBehaviour {
 
     private Movement movement; 
+    private Transform playerTransform;
+    private BoxCollider2D boxCollider; 
+    public LayerMask layerMask;
+    private Player player;
 
     void Start() {
-        this.movement = this.transform.parent.GetComponent<Movement>();
+        this.playerTransform = this.transform.parent;
+        this.movement = this.playerTransform.GetComponent<Movement>();
+        this.boxCollider = this.playerTransform.GetComponent<BoxCollider2D>();
+        this.player = this.playerTransform.GetComponent<Player>();
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.transform.tag == "Platform") {
-            this.movement.isTouchingGround = true;
-        }
-    }
+    void Update() {
+        float y = this.boxCollider.size.y;
+        float x = this.boxCollider.size.x;
+        RaycastHit2D hitDownLeft = Physics2D.Raycast(playerTransform.position + new Vector3(-x/2, 0f, 0f), -playerTransform.up, (3*y)/4, layerMask);
+        RaycastHit2D hitDownRight = Physics2D.Raycast(playerTransform.position + new Vector3(x/2, 0f, 0f), -playerTransform.up, (3*y)/4, layerMask);
+        Debug.DrawRay(playerTransform.position + new Vector3(-x/2, 0f, 0f), -playerTransform.up * (3*y)/4, Color.red);
+        Debug.DrawRay(playerTransform.position + new Vector3(x/2, 0f, 0f), -playerTransform.up * (3*y)/4, Color.green);
 
-    private void OnTriggerExit2D(Collider2D other) {
-        if (other.transform.tag == "Platform") {
-            this.movement.isTouchingGround = false;
-        }
+        this.movement.isTouchingGround = hitDownLeft.collider != null || hitDownRight.collider != null;
+
     }
 }
